91ed32b5f6f96b3a40952236ec8021aac5dfa955
Dhvnrmdhn <Dhevanramadhangaming@gmail.com>
2023-01-10 23:01:51 +0700

    .

---
+++ date.php
@@ -0,0 +1,20 @@
+<?php
+    // Date
+    // Menampilkan tanggal dengan format tertentu
+    // echo date("M");
+
+    // Time
+    // UNIX Timestamp / EPOCH Time
+    // Detik yang sudah berlalu sejak 1 Januari 1970
+    // echo time();
+    // echo date("l", time()-60*60*24*100);
+
+    // mktime
+    // membuat sendiri detik
+    // mktime(0,0,0,0,0,0)
+    // jam, menit, detik, bulan, tanggal, tahun
+    // echo date("l", mktime(0,0,0,2,11,2005));
+
+    // strtotime
+    // echo date ("l", strtotime("2 Nov 2005"));
+?>
